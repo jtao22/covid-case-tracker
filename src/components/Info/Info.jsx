8c7ad7d6cfbe8b88ee9f7ebf74f97ed1f28d616b
Jonathan Tao <jellybellyjonny@gmail.com>
2021-02-13 14:31:45 -0800

    info folder updates

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import styles from './Info.module.css';
+import {Card,CardContent,Typography,Grid} from '@material-ui/core';
+import cx from 'classnames';
 
-const Info = () => {
-
+const Info = ({data:{ confirmed ,recovered, deaths, lastUpdate }}) => {
+    if(!confirmed){
+        return 'Preparing Information...';
+    }
     return(
-        <h1>hi</h1>
-    )
+        <div className = {styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card} className = {cx(styles.card, styles.infected)}>
+                    <CardContent>
+                        <Typography color = "textSecondary" gutterBottom> Contracted</Typography>
+                        <Typography variant = "h6" >data</Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography variant="body2">Total cases</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card} className = {cx(styles.card, styles.recovered)}>
+                    <CardContent>
+                        <Typography color = "textSecondary" gutterBottom> Recovered</Typography>
+                        <Typography variant = "h6" >data</Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography variant="body2">Total recovered</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card} className = {cx(styles.card, styles.deaths)}>
+                    <CardContent>
+                        <Typography color = "textSecondary" gutterBottom> Deaths</Typography>
+                            <Typography variant = "h6" ></Typography>
+                            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography variant="body2">Total deaths</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>    )
 }
 
 export default Info;
